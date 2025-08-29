018d7f662c9d69c6f7516b95658ca77d378f01a2
LivinkumarS <001liv100@ggmail.com>
2025-08-29 12:33:15 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,36 @@
+'''This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number
+This below code is to printout some number'''
+# print(1)
+# print(12)
+# PRINT(123)
+
+# print("Hello World!")
+# print('I love the following quote "The qoute!"')
+
+# a=input() # "12"
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+
+a=10
+b=16
+a=b
+print(a)
