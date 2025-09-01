c06092396cb91dadb0bafce02c1cee80dcbb71ec
LivinkumarS <001liv100@ggmail.com>
2025-09-01 14:07:45 +0530

    Variables, Datatyes and Type casting

---
+++ VariablesDatatypeTypeCasting.py
@@ -0,0 +1,25 @@
+a=10
+# *A=20
+# print(a)
+
+# print1=10
+# print(print1)
+
+# b=5.45
+# print(b)
+
+# str1="Hello"
+# str2="Hi"
+# print(str1+str2)
+
+# bool1=True
+# print(bool1)
+
+
+name="Surya"
+age=50
+
+print("his name is "+name+". he is "+str(age)+" years old!")
+print(f"his name is {name}. he is {age} years old")
+
+
