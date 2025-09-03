b86e5fd292eb89d022fb271905037f91a4059d77
LivinkumarS <001liv100@ggmail.com>
2025-09-03 14:11:45 +0530

    Control Flow

---
+++ ControlFlow.py
@@ -0,0 +1,63 @@
+# inp=input("Enter age: ")
+# inp>=18
+# print("You can vote")
+# print("You can't vote")
+
+
+# a=10
+
+# if a>10:
+#     print("From If block!")
+# elif a==10:
+#     print("From Elif1 Block")
+# elif a<10:
+#     print("From Elif2 Block")
+# elif a>-10:
+#     print("From Elif3 Block")
+# else:
+#     print("From Else block!")
+
+
+# age=int(input("Enter Your Age: "))
+
+# if(age>=18):
+#     print("Can vote")
+# else:
+#     print("Cannot vote")
+
+# Input: marks from user
+# marks = float(input("Enter the marks: "))
+
+# # Grading using if-elif-else
+# if 91 <= marks <= 100:
+#     grade = 'O'
+# elif 81 <= marks < 91:
+#     grade = 'A+'
+# elif 71 <= marks < 81:
+#     grade = 'A'
+# elif 61 <= marks < 71:
+#     grade = 'B+'
+# elif 51 <= marks < 61:
+#     grade = 'B'
+# elif 41 <= marks < 51:
+#     grade = 'C'
+# elif 0 <= marks <= 40:
+#     grade = 'Fail'
+# else:
+#     grade = 'Invalid Marks'
+
+# print("Grade:", grade)
+
+
+num=int(input("Number: "))
+
+if(num>10):
+    if num>15:
+        print("Very large number")
+    else:
+        print("Large number")
+else:
+    if num<5:
+        print("Very Smaller number")
+    else:
+        print("smaller number")
