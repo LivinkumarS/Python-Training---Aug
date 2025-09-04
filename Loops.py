05ac3bb13e1d2c5b863e192384f597e0567e57dd
LivinkumarS <001liv100@ggmail.com>
2025-09-04 13:08:42 +0530

    Loops

---
+++ Loops.py
@@ -0,0 +1,39 @@
+# a=0 
+
+# while a<10:
+#     print("Hello world"+str(a+1))
+#     a+=1
+
+# a="Hello world"
+
+# for i in a:
+#     print(i)
+
+
+# for i in range(5,10):
+#     print(i)
+
+# for num in range(1,101,5):
+#     print(num)
+
+# for i in range(50):
+#     if i==30:
+#         continue
+#     print(i)
+#     print("-----------")
+
+# for i in range(50):
+#     print("hi")
+
+# secret=19
+
+# while True:
+#     guessedNum=int(input("Guess the number: "))
+#     if(guessedNum==secret):
+#         print("You won")
+#         break
+#     else:
+#         print("Try again")
+
+# print("Game Over!")
+
