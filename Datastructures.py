a49f2a8683aa6601834662ad463e8df116641a93
LivinkumarS <001liv100@ggmail.com>
2025-09-08 13:31:05 +0530

    Datastructures

---
+++ Datastructures.py
@@ -0,0 +1,102 @@
+# b="Hello world"
+
+# print(b[3:9])
+# print(a[0])
+# print(a[2:])
+# print(a[:6])
+# print(a[3:6])
+
+# a[4]=4
+
+# a.append(True)
+# a.append(True)
+# a.insert(3,True)
+
+a=[1,2,3,"four","five",True,False,3,3,3,3]
+
+# a.remove(3)
+# a.remove(3)
+
+# a.pop(3)
+# a.pop()
+# a.pop()
+# a.pop()
+# a.pop()
+
+# print(a)
+
+# lis1=[23,43,45,32,6,42,64,42,76,98,65,57,42,43,56,58]
+# lis1.sort()
+# lis1.reverse()
+# print(lis1.count(500))
+# print(lis1.index(40))
+
+lis1=[
+    1,
+    2,
+    3,
+    [
+        4,
+        5,
+        6,
+        [
+            "seven",
+            "eight",
+            "nine"
+        ]
+    ]
+]
+
+print(lis1[-1][-1][-1][-1])
+
+
+# tup1=(1,2,3,4,5,4,5)
+# # tup1[2]=20
+# print(tup1[2])
+
+# person1=("Kane",22,182)
+
+# name,age,height=person1
+
+# print(name)
+# print(age)
+# print(height)
+
+# set1={
+#     1,2,3,4,5,55,56
+# }
+
+# set2={
+#     1,2,3,4,5,5,6,7,8
+# }
+
+# print(set1.union(set2))
+# print(set2.difference(set1))
+
+# dict1={
+#     "name":"Kane",
+#     "age":22,
+#     "height":182.5,
+#     "role":"Dev",
+#     "isMarried":False,
+#     1:"one"
+# }
+
+# dict1["height"]=179.5
+
+# print(list(dict1.keys()))
+# print(list(dict1.values()))
+
+# print(dict1.items())
+
+# dict1.pop(1)
+# dict1.clear()
+
+# print(dict1)
+
+
+# arr=[32,324,234,234,32,4,4,
+#      432,432,5,234,132,
+#      1,1,1,1,1,1,34,3,34,43]
+
+# print(list(set(arr)))
