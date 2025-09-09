36a114f22204dc9647c4152b662c996c4e7ef2ec
LivinkumarS <001liv100@ggmail.com>
2025-09-09 13:56:57 +0530

    String methods

---
+++ String.py
@@ -0,0 +1,28 @@
+
+# print(a[8:])
+# print(a*7)
+
+# print("o W" not in a)
+
+# ans=a.upper()
+# ans=a.lower()
+# ans=a.title()
+
+# ans=a.isalpha()
+# ans=a.isdigit()
+
+# ans=a.find("o wod")
+# ans=a.replace("Hello","Hi")
+# ans=ans.replace("world","tamilnadu")
+
+# a="I would like to play cricket!"
+# lis=['I', 'would', 'like', 'to', 'play', 'cricket!']
+
+# ans=a.split(' ')
+# ans= "".join(lis)
+
+# print(ans)
+
+a="      Hello world!       "
+print(a)
+print(a.strip())
