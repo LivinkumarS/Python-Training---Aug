8a8f7a679a73d7753625647424957bff5a4b7014
LivinkumarS <001liv100@ggmail.com>
2025-09-10 13:40:33 +0530

    File Handling

---
+++ fileHandling/FileHandling.py
@@ -0,0 +1,27 @@
+# file= open("file.txt",'r')
+
+# ans=file.read()
+# # ans=file.readline()
+# # ans=file.readline()
+# # ans=file.readline()
+# print(ans)
+
+# file.close()
+
+# file=open("file.txt",'w')
+
+# content="Hello.\nHi"
+# # print(content)
+# file.write(content)
+
+
+# file.close()
+
+file=open("file.txt",'a')
+
+content="\nHello.\nHi"
+# print(content)
+file.write(content)
+
+
+file.close()
